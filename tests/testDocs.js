e0d0d232cb3d9948fbf69f6d03aff92bc6ed52a1
Tobenna Ndu <tobenna.ndu@holidayextras.com>
2017-04-28 10:52:06 +0100

    Removed unused tests

--- tests/testDocs.js
+++ tests/testDocs.js
@@ -49,30 +49,6 @@ describe('Unit - docs', function(){
       });
 
     });
-
-    context('With good input and subdirectories', function(){
-      var processDocStub = null;
-
-      beforeEach(function() {
-        sinon.stub(docs, '_processSubdirectories').yields(['a', 'b', 'c']);
-      });
-
-      afterEach(function(){
-        docs._processSubdirectories.restore();
-      });
-
-      it('should call _processSubdirectories for with the provided array', function() {
-        docs.get(opts, callback);
-        assert.ok(docs._processSubdirectories.calledWith(['a', 'b', 'c']));
-      });
-
-      it('should call async.each', function(){
-        sinon.stub(async, 'each');
-        docs.get(opts, callback);
-        assert.ok(async.each.calledOnce);
-        async.each.restore();
-      });
-    });
   });
 
   describe('_processDocument', function(){
@@ -109,95 +85,50 @@ describe('Unit - docs', function(){
     });
 
     it('should callback content if empty rules object', function(){
-      getDocStub.callsArgWith(2, null, {
+      getDocStub.callsArgWith(2, null, [{
         content: "My content!",
         rules: {}
-      });
+      }]);
       docs._processDocument('somedoc', '2015-01-01 00:00:00', {}, callback);
       assert.equal(callback.firstCall.args[1], 'My content!');
     });
 
     it('should callback content if empty rules object', function(){
-      getDocStub.callsArgWith(2, null, {
+      getDocStub.callsArgWith(2, null, [{
         content: "My content!",
         rules: {
           rule1: 'a',
           rule2: 'b'
         }
-      });
+      }]);
       docs._processDocument('somedoc', '2015-01-01 00:00:00', {}, callback);
       assert.ok(docs._runDocumentRules.calledOnce);
     });
 
     it('should callback content if _runDocumentRules returns true', function(){
-      getDocStub.callsArgWith(2, null, {
+      getDocStub.callsArgWith(2, null, [{
         content: "My content!",
         rules: {
           rule1: 'a',
           rule2: 'b'
         }
-      });
+      }]);
       docRulesStub.returns(true);
       docs._processDocument('somedoc', '2015-01-01 00:00:00', {}, callback);
       assert.equal(callback.firstCall.args[1], 'My content!');
     });
 
     it('should callback empty string if _runDocumentRules returns false', function(){
-      getDocStub.callsArgWith(2, null, {
+      getDocStub.callsArgWith(2, null, [{
         content: "My content!",
         rules: {
           rule1: 'a',
           rule2: 'b'
         }
-      });
+      }]);
       docRulesStub.returns(false);
       docs._processDocument('somedoc', '2015-01-01 00:00:00', {}, callback);
       assert.equal(callback.firstCall.args[1], '');
-    });
-  });
-
-  describe('_processSubdirectories', function() {
-    var callback = null;
-    var paragraphCheck = null;
-    var parentDirectories;
-
-    beforeEach(function() {
-      callback = sinon.spy();
-      paragraphCheck = sinon.stub(docs.connector, 'checkForParagraphs');
-      parentDirectories = ['a'];
-    });
-
-    afterEach(function(){
-      callback = null;
-      docs.connector.checkForParagraphs.restore();
-    });
-
-    context('when no parent directories are provided', function() {
-      it('should return an empty array', function(){
-        var documents = docs._processSubdirectories(null);
-        assert.deepEqual(documents, []);
-      });
-    });
-
-    context('when we have parent directories', function() {
-      it('should call async.each', function(){
-        sinon.stub(async, 'each');
-        docs._processSubdirectories(parentDirectories);
-        assert.ok(async.each.calledOnce);
-        async.each.restore();
-      });
-
-      it('should call connector.checkForParagraphs', function(){
-        docs._processSubdirectories(parentDirectories);
-        assert.ok(docs.connector.checkForParagraphs.calledOnce);
-      });
-
-      it('should return the just the sub directories for this parent if the database returns them', function(){
-        paragraphCheck.yields(null, [ { type: 'a/a_sub_document_one' }]);
-        var expected = [[ { type: 'a/a_sub_document_one' }]];
-        docs._processSubdirectories(parentDirectories, callback);
-        assert.deepEqual(callback.firstCall.args[1], expected);
-      });
     });
   });
 
